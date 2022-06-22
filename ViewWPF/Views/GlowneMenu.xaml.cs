835eb50339598d3f08b886ec738c0e6aa6df800d
Piotr <piotr.zywczak@microsoft.wsei.edu.pl>
2022-06-22 17:21:53 +0200

    Wprowadzenie oknien do wizyt

--- ViewWPF/Views/GlowneMenu.xaml.cs
+++ ViewWPF/Views/GlowneMenu.xaml.cs
@@ -60,5 +60,15 @@ namespace ViewWPF.Views
             ListaPacjentow pc = new ListaPacjentow();
             pc.Show();
         }
+        private void ListaWizyt_Click(object sender, RoutedEventArgs e)
+        {
+            ListaLekarzy mc = new ListaLekarzy();
+            mc.Show();
+        }
+
+        private void zarejestrujWizyte_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
